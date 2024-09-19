98eef1eee526ae89f2bf53635a98a6a923d6b414
pentacle8 <t.andodev@gmail.com>
2024-09-20 00:25:17 +0900

    add dark theme

--- assets/js/theme-toggle.js
+++ assets/js/theme-toggle.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const currentTheme = 'dark';
+    document.documentElement.setAttribute('data-theme', currentTheme);
+  
     const toggleButton = document.querySelector('.theme-toggle');
     toggleButton.addEventListener('click', function() {
-      const currentTheme = document.documentElement.getAttribute('data-theme');
       const newTheme = currentTheme === 'light' ? 'dark' : 'light';
       document.documentElement.setAttribute('data-theme', newTheme);
     });
   });
+  
